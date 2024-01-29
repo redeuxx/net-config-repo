82cdccc2edf4653d1b3b6285ddbc3423125f6b50
Vernon Wenberg <vernon@wenberg.net>
2024-01-29 15:38:10 -0600

    Refactor manage.py to use argparse.

--- manage.py
+++ manage.py
@@ -16,49 +16,46 @@ parser.version = conf.VERSION
 
 # You must choose one of the mutually exclusive options.
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-s", "--scan", help='Scan an IP address or CIDR for hosts that are alive.')
-group.add_argument("-l", "--list", help='List all devices in the database.')
-group.add_argument("-a", "--add", help='Add a device to the database.')
-# end group
+group.add_argument("--scan", help='Scan an IP address or CIDR for hosts that are alive.')
+group.add_argument("--list", action="store_true", help='List all devices in the database.')
+group.add_argument("--add", help='Add a device to the database.')
+group.add_argument("--remove", type=int, help='Delete a device from the database using the device id.')
+group.add_argument("--fetchall", action="store_true", help='Fetch all configs from the database.')
+group.add_argument("--clean", action="store_true", help='Clean up running-configs/ directory.')
 
+# end group
+parser.add_argument("-hostname", help='Hostname of the device.')
+parser.add_argument("-type", help='Type of device.')
 parser.add_argument("-v", "--version", action='version', help='Show the version of the program.')
+
 args = parser.parse_args()
 
 if args.scan:
     hosts.scan_cidr(args.scan)
 elif args.list:
-    for ip, device_id in zip(db.list_all_ips()[0], db.list_all_ips()[1]):
-        print(f"{device_id} - {ip}")
+    if(len(db.list_all_ips()[0])) == 0:
+        print("No devices in database.")
+    else:
+        for ip, device_id in zip(db.list_all_ips()[0], db.list_all_ips()[1]):
+            print(f"{device_id} - {ip}")
 elif args.add:
-    db.insert_device(ip=args.add, hostname="", device_type="")
-
-# TODO: convert this to argparse
-# if len(sys.argv) < 2:
-#     OPTION = "default"
-# else:
-#     OPTION = sys.argv[1]
-
-# match OPTION:
-#     case "list":
-#         # iterate through db.list_all_ips() and list 0 and 1 indexes of the list
-#         for ip, device_id in zip(db.list_all_ips()[0], db.list_all_ips()[1]):
-#             print(f"{device_id} - {ip}")
-#     case "add":
-#         # add device to db. Takes 2 arguments: ip, device_type
-#         # TODO: there really needs to be syntax checking here
-#         db.insert_device(ip=sys.argv[2], hostname="", device_type=sys.argv[3])
-#     case "del":
-#         # delete device by id
-#         if db.delete_device(id=sys.argv[2]) == 1:
-#             print("Device deleted.")
-#         else:
-#             print("Device ID does not exist.")
-#     case "fetchall":
-#         # fetch all configs
-#         get_config.fetch_all_configs()
-#     case "clean":
-#         # clean up running-configs/ directory
-#         print("Cleaning up running-configs/ directory.")
-#         utils.del_oldest_configs(conf.MAX_CONFIGS)
-#     case _:  # default
-#         print("Valid options are: list, add, del, fetchall, clean")
+    if (args.hostname is not None):
+        hostname = args.hostname
+    else:
+        hostname = ""
+        
+    if(args.type is None):
+        print("You must specify a device type with the -type flag.")
+    else:
+        db.insert_device(ip=args.add, hostname="", device_type=args.type)
+        print("Device added.")
+elif args.remove:
+    if db.delete_device(id=args.remove) == 1:
+        print("Device removed.")
+    else:
+        print("Device ID does not exist.")
+elif args.fetchall:
+    get_config.fetch_all_configs()
+elif args.clean:
+    print("Cleaning up running-configs/ directory.")
+    utils.del_oldest_configs(conf.MAX_CONFIGS)
