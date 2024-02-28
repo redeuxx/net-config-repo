ef59778b33b31be113dd1f51b04562bca0c6230c
Vernon Wenberg <vernon@wenberg.net>
2024-02-28 01:24:49 -0600

    - Raise connection timeout. - Use pathlib instead of os.

--- conf.py
+++ conf.py
@@ -1,5 +1,9 @@
-import os
+import pathlib
 
 VERSION = "0.0.1"
-STORE_DIR = os.path.abspath(os.path.join(os.getcwd(), "running-configs/"))
+# Use pathlib to specify the path to store the running-configs, see https://docs.python.org/3/library/pathlib.html
+# This default path will point to the user home directory, under a folder called "running-configs"
+# For example, in Windows, the path will be C:\Users\username\running-configs and in Linux,
+# /home/username/running-configs
+STORE_DIR = pathlib.Path.home() / "running-configs/"
 MAX_CONFIGS = 10
